a51a698487daaefe11e719313ace37e150b61132
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-08 17:15:35 +0200

    Add no-markup variant of dictionary

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -20,3 +20,32 @@ def test_default_dictionary_has_expected_content() -> None:
 
     assert result[25000].word == "undanhald"
     assert result[25000].definitions[0] == "n. <i>flight</i>."
+
+
+def test_no_markup_dictionary_has_correct_length() -> None:
+    result = get_dictionary(DictionaryPath.NO_MARKUP)
+    assert len(result) == 29951
+
+
+def test_no_markup_dictionary_has_expected_content() -> None:
+    result = get_dictionary(DictionaryPath.NO_MARKUP)
+
+    assert result[14].word == "afbindi"
+    assert result[14].definitions[0] == "n. constipation."
+
+    assert result[1000].word == "andstreymr"
+    assert (
+        result[1000].definitions[0]
+        == "a. strongly adverse (andstreym ørlög); Sighvatr var heldr ~ um eptirmálin, hard to come to terms with."
+    )
+
+    assert result[25000].word == "undanhald"
+    assert result[25000].definitions[0] == "n. flight."
+
+
+def test_dictionary_headwords_match() -> None:
+    dictionary = get_dictionary(DictionaryPath.DEFAULT)
+    no_markup_dictionary = get_dictionary(DictionaryPath.NO_MARKUP)
+
+    for index, entry in enumerate(dictionary):
+        assert entry.word == no_markup_dictionary[index].word
